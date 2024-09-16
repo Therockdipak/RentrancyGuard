4495736c546dece0a7c8dc3fa692d1d246d62033
Dipak Bansod <108859739+Therockdipak@users.noreply.github.com>
2024-09-16 20:45:35 +0530

    Update deploy.js

--- ignition/modules/deploy.js
+++ ignition/modules/deploy.js
@@ -1,8 +1,5 @@
 const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
-const JAN_1ST_2030 = 1893456000;
-const ONE_GWEI = 1_000_000_000n;
-
 module.exports = buildModule("velnerableBank", (m) => {
  
   const lock = m.contract("VelnerableBank", []);
